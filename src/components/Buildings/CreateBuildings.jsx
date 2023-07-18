99f06df327953ff7fb0b832c20861d8fbc0f7471
Jimmy <131156471+Jimbow-cl@users.noreply.github.com>
2023-07-18 13:17:02 +0200

    Create Building

--- src/components/Buildings/CreateBuildings.jsx
+++ src/components/Buildings/CreateBuildings.jsx
@@ -1,2 +1,37 @@
-function Cbuilding
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
+function Cbuilding(){
+    const [building, setBuilding] = useState();
+    function Create(){
+        var requestOptions = {
+            method: 'POST',
+            redirect: 'follow'
+          };
+          
+          fetch(`http://127.0.0.1:8000/api/structures/${building}`, requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+            
+    }
+       
+
+   
+
+    return (
+    <div>
+        <Form.Select  onChange={(e) => setBuilding(e.target.value)} aria-label="Default select example">
+            <option>Select your Building</option>
+            <option value="mine">Mine</option>
+            <option value="raffinery">Raffinery</option>
+            <option value="powerplant">PowerPlant</option>
+            <option value="shipyard">Shipyard</option>
+        </Form.Select>
+        <Button type="submit" onClick={Create}>Create</Button>
+    </div>
+    );
+}
+
+export default Cbuilding
--- src/pages/BuildYourEmpire.jsx
+++ src/pages/BuildYourEmpire.jsx
@@ -1,11 +1,8 @@
 import Header from "../models/ModelsHeader";
 import Footer from "../models/ModelsFooter";
 import Menu from "../layout/Menu";
-import Row from 'react-bootstrap/Row';
-import Tab from 'react-bootstrap/Tab';
-import Col from 'react-bootstrap/Col';
 import Buildings from "../components/buildings/ViewBuildings";
-import ListGroup from 'react-bootstrap/ListGroup';
+import Cbuilding from "../components/Buildings/CreateBuildings";
 
 
 function BuildYourEmpire (){
@@ -15,15 +12,8 @@ function BuildYourEmpire (){
             <Header/>
             <Menu/>
             <h1>Build Youre Empire!</h1>
-            <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
-                <Row>
-                    <Col sm={4}>
-                        <ListGroup>
-                            <Buildings/>
-                        </ListGroup>
-                    </Col>
-                </Row>
-            </Tab.Container>
+             <Cbuilding/>
+                            <Buildings/>         
             <Footer/>
         </div>
     )
