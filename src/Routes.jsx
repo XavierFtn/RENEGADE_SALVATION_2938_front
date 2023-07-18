1df85da15f7400620edcc41029b60c7280b32f63
Montigny Gérald <131920955+Slam85@users.noreply.github.com>
2023-07-18 11:18:56 +0200

    Route create

---
+++ src/Routes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./Components/Pages/Home";
+import Login from "./Components/Pages/Login";
+import CreateAccount from "./Components/Pages/CreateAccount";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/Create",
+    element: <CreateAccount />,
+  },
+]);
