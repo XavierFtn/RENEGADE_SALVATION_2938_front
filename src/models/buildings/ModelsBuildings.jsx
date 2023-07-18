70813a3472cc96ac11e8a175321bdcfe31cb32fd
Jimmy <131156471+Jimbow-cl@users.noreply.github.com>
2023-07-18 14:09:35 +0200

    Moment

--- src/models/buildings/ModelsBuildings.jsx
+++ src/models/buildings/ModelsBuildings.jsx
@@ -1,9 +1,32 @@
 
-import { Button } from 'react-bootstrap';
+import moment from 'moment/moment';
+import { useEffect, useRef } from 'react';
+import { Button, ProgressBar } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 
 function MBuildings(props){
+  
+  //Utilisation de useRef pour afficher la possibilité de Supprimer ou pas si
+  // le Batiment est encore en construction.
+  const wait = useRef()
+  const finish = useRef()
+  function Display() {
+  var now =  moment().format('YYYY-MM-DD hh:mm ');
+  var build = moment(props.created_at).add(1,'hours').hours();
 
+  if (now > build){var time = false;}
+  else {time = true;}
+
+    if ( time == true) {
+        wait.current.style.display = "none";
+        finish.current.style.display = "block";   
+    }
+    else {
+        wait.current.style.display = "block";
+        finish.current.style.display = "none";
+    }
+}
+  useEffect(() => { Display() },)
     return (
     <div>
       <Card border="danger" style={{ width: '18rem' }}>
@@ -12,8 +35,13 @@ function MBuildings(props){
           <Card.Title>level: {props.level}</Card.Title>
           <Card.Text> Energy Consumption: {props.energy_consumption}
           </Card.Text>
-          <Button variant="primary">Learn More</Button>
-          <Button variant="danger">Delete</Button>
+            <Button variant="primary">Learn More</Button>
+          <div ref={finish}>
+            <Button variant="danger">Delete</Button>
+          </div >
+          <div ref={wait}>
+          <ProgressBar animated now={45} />
+          </div>
         </Card.Body>
       </Card>
       <br />
