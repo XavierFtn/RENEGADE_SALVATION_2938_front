a1eb6fe04f67cb3007241de4163fe59bc79fea91
DiogoCosta82 <132056422+DiogoCosta82@users.noreply.github.com>
2023-07-19 15:04:48 +0200

    MJ page register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,9 +27,12 @@ function Login() {
       const data = await response.json();
 
       if (data.status === "success") {
-        localStorage.setItem("token", JSON.stringify( data.authorisation.token));
-        localStorage.setItem("user", JSON.stringify( data.user.firstname))
-        localStorage.setItem("planet", JSON.stringify( data.user.planetary_system_id))
+        localStorage.setItem("token", JSON.stringify(data.authorisation.token));
+        localStorage.setItem("user", JSON.stringify(data.user.firstname));
+        localStorage.setItem(
+          "planet",
+          JSON.stringify(data.user.planetary_system_id)
+        );
         swal("Welcome!", "You are connected!", "success");
         navigate("/");
       } else {
