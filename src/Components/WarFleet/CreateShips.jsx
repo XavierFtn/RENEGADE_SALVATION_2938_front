98354bb987aadf7e457bb69aa92ed83fa6702efd
XavierFtn <xavier.fontaine072@gmail.com>
2023-07-19 22:09:29 +0200

    Components ExtendMyEmpire

--- src/Components/WarFleet/CreateShips.jsx
+++ src/Components/WarFleet/CreateShips.jsx
@@ -21,4 +21,24 @@ function Cships() {
       .then((result) => console.log(result))
       .catch((error) => console.log("error", error));
   }
+
+  return (
+    <div>
+      <Form.Select
+        onChange={(e) => setShips(e.target.value)}
+        aria-label="Default select example"
+      >
+        <option>Select your ship</option>
+        <option value="fighter">Mine</option>
+        <option value="frigate">Raffinery</option>
+        <option value="cruiser">PowerPlant</option>
+        <option value="destroyer">Shipyard</option>
+      </Form.Select>
+      <Button type="submit" onClick={Create}>
+        Create
+      </Button>
+    </div>
+  );
 }
+
+export default Cships;
--- src/pages/ExtendYourEmpire.jsx
+++ src/pages/ExtendYourEmpire.jsx
@@ -2,7 +2,7 @@ import Header from "../models/ModelsHeader";
 import Footer from "../models/ModelsFooter";
 import Menu from "../layout/Menu";
 import ListGroup from "react-bootstrap/ListGroup";
-import Battleresult from "../Components/img/Battleresult";
+
 import { Link } from "react-router-dom";
 
 function ExtendYourEmpire() {
