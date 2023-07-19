8b52c9ad8a5f551f0242cc8faa437f9e177aa6cd
Jimmy <131156471+Jimbow-cl@users.noreply.github.com>
2023-07-19 12:25:03 +0200

    token ok

--- src/components/Buildings/ViewBuildings.jsx
+++ src/components/Buildings/ViewBuildings.jsx
@@ -5,18 +5,11 @@ import { useEffect, useState } from 'react';
 
 function Buildings(){
     const [building, setBuilding] = useState([]);
-    const [token, setToken] = useState(["eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL2xvZ2luIiwiaWF0IjoxNjg5NzUzNTg5LCJleHAiOjE2ODk3NTcxODksIm5iZiI6MTY4OTc1MzU4OSwianRpIjoic2JpYkxZWU5YaFBQck11byIsInN1YiI6IjEiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.9HH5xgMKcRVialISDUpBOyGl7BcvGVAPhXzPGo7U95c"]);
-
-    useEffect(() => {
-      const items = JSON.parse(localStorage.getItem('token'));
-      if (items) {
-       setToken(items);
-      }
-    }, []);
 
     async function Mbuilding1(){
         var myHeaders = new Headers();
-        myHeaders.append("Authorization", `Bearer ${token} `);
+        const items = JSON.parse(localStorage.getItem('token'));
+        myHeaders.append("Authorization", `Bearer ${items} `);
 
         var requestOptions = {
             method: 'GET',
