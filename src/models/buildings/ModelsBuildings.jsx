67b24ac71d299a868eadba805ac452d5288b9d2f
Jimmy <131156471+Jimbow-cl@users.noreply.github.com>
2023-07-19 21:25:05 +0200

    Filter Map

--- src/models/buildings/ModelsBuildings.jsx
+++ src/models/buildings/ModelsBuildings.jsx
@@ -1,16 +1,3 @@
-/**
-    * @description      : 
-    * @author           : 
-    * @group            : 
-    * @created          : 19/07/2023 - 13:59:22
-    * 
-    * MODIFICATION LOG
-    * - Version         : 1.0.0
-    * - Date            : 19/07/2023
-    * - Author          : 
-    * - Modification    : 
-**/
-
 import moment from 'moment/moment';
 import { useEffect, useState } from 'react';
 import { Button, ListGroup, Modal, ProgressBar } from 'react-bootstrap';
@@ -31,7 +18,6 @@ function MBuildings(props){
   const handleShow = () => setShow(true);
 
   function Display() {
-
     var now = moment();
     var build = moment(props.created_at).add(1, 'hours'); // Temps du created_at avec 1heure supp
     setBuildMin(build.diff(now, 'minutes')); // Temps restant en minutes
@@ -49,13 +35,14 @@ function MBuildings(props){
 function handleDeleteClick() {
   props.onDelete(); // Props de la fonction de suppression
 }
-  useEffect(() => { Display() },)
+  useEffect(() => {  Display() },)
   
     return (
     <div>
-      <Card bg ="dark" text="light" className="text-center" style={{ width: '18rem' }}>
+      <Card bg ="dark" text="light" className="text-center p-0" style={{ width: '15rem' }}>
         <Card.Body>
-          <Card.Title>{props.type} n°{props.id} level: {props.level} </Card.Title>         
+          <Card.Title className=" m-0">{props.type} n°{props.id} </Card.Title>
+          <Card.Title className=" m-0">level: {props.level} </Card.Title>             
           <ListGroup >
           {showWait &&<div>
               <Button onClick={handleShow}   variant="danger">Delete</Button>
@@ -63,7 +50,7 @@ function handleDeleteClick() {
                 <Modal.Header closeButton>
                   <Modal.Title>*Laser Noise*</Modal.Title>
                   </Modal.Header>
-                <Modal.Body>Are you sure to delete this {props.type} ?</Modal.Body>
+                <Modal.Body>Are you sure to delete this {props.type} n° {props.id}  ?</Modal.Body>
                 <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>Close</Button>
                 <Button variant="danger" onClick={() => { handleClose(); handleDeleteClick(); }}>Delete</Button>        </Modal.Footer>
--- src/pages/BuildYourEmpire.jsx
+++ src/pages/BuildYourEmpire.jsx
@@ -1,19 +1,8 @@
-/**
-    * @description      : 
-    * @author           : 
-    * @group            : 
-    * @created          : 19/07/2023 - 13:51:06
-    * 
-    * MODIFICATION LOG
-    * - Version         : 1.0.0
-    * - Date            : 19/07/2023
-    * - Author          : 
-    * - Modification    : 
-**/
+
 import Header from "../models/ModelsHeader";
 import Footer from "../models/ModelsFooter";
 import Buildings from "../Components/Buildings/ViewBuildings";
-import Cbuilding from "../components/Buildings/CreateBuildings";
+import Cbuilding from "../Components/Buildings/CreateBuildings";
 import {  Card } from "react-bootstrap";
 
 
@@ -22,13 +11,15 @@ function BuildYourEmpire (){
     return(
         <div className="container-fluid">
             <Header/>
-            <div className="row justify-content-center">
-                <h1 className="orbitron">Build Your Empire!</h1>
+            <div className="row mt-5 pt-2" >
+                <div className="col d-flex justify-content-center">
+                    <h1 className="orbitron">Build Your Empire!</h1>
+                </div>
             </div>
             <div className="row wrap">
                 <div className="col-md-6">
                 <Card className="text-center">
-                    <Card.Header><h1 className="orbitron">Create</h1></Card.Header>
+                    <Card.Header className="py-0 "><h1 className="orbitron">Create</h1></Card.Header>
                     <Card.Body>
                     <Cbuilding/>
                     </Card.Body>
@@ -36,7 +27,7 @@ function BuildYourEmpire (){
                 </div>
                 <div className="col-md-6">          
                     <Card className="text-center">
-                        <Card.Header><h1 className="orbitron">List of Ressources</h1></Card.Header>
+                        <Card.Header className="py-0 "><h1 className="orbitron">List of Ressources</h1></Card.Header>
                         <Card.Body>
                          <Cbuilding/>        
                         </Card.Body>
@@ -46,39 +37,42 @@ function BuildYourEmpire (){
             <div className="row mb-3"></div>
             <div className="row wrap">
                 <div className="col-md-3 ">
-                    <Card className="text-center px-0">
-                        <Card.Header><h1 className="orbitron">Mine</h1></Card.Header>
+                    <Card className="text-center px-0 pt-2">
+                        <Card.Header className="py-0 "><h1 className="orbitron">Mine</h1></Card.Header>
                         <Card.Body >
                             <div className="scroll" >
-                                <Buildings/> 
+                            <Buildings type={'mine'} />
                             </div>
                         </Card.Body>
                     </Card>
                 </div>
                 <div className="col-md-3">
-                    <Card className="text-center px-0">
-                        <Card.Header><h1 className="orbitron">PowerPlant</h1></Card.Header>
+                    <Card className="text-center px-0 pt-2">
+                        <Card.Header className="py-0 "><h1 className="orbitron">PowerPlant</h1></Card.Header>
                         <Card.Body>     
                             <div className="scroll" >
+                            <Buildings type={'powerplant'} />
                             </div>   
                         </Card.Body>
                     </Card>
                 </div>
                 <div className="col-md-3">
-                        <Card className="text-center px-0">
-                            <Card.Header><h1 className="orbitron">raffinery</h1></Card.Header>
+                        <Card className="text-center px-0 pt-2">
+                            <Card.Header className="py-0 "><h1 className="orbitron">raffinery</h1></Card.Header>
                             <Card.Body>
                                 <div className="scroll" >
+                                <Buildings type={'raffinery'} />
                                 </div>           
                             </Card.Body>
                         </Card>
                 </div>
                 <div className="col-md-3">
                     
-                        <Card className="text-center px-0">
-                            <Card.Header><h1 className="orbitron">Shipyard</h1></Card.Header>
+                        <Card className="text-center px-0 pt-2">
+                            <Card.Header className="py-0 "><h1 className="orbitron">Shipyard</h1></Card.Header>
                             <Card.Body>
                                 <div className="scroll" >
+                                <Buildings type={'shipyard'} />
                                 </div>           
                             </Card.Body>
                         </Card>
