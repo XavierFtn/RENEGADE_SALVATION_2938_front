87bce139cdeb45ac43c3f085ff3349c509f2a7be
XavierFtn <xavier.fontaine072@gmail.com>
2023-07-21 14:33:18 +0200

    modif

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./layout/Router";
 import "../src/Components/style/home.css";
-import Counter from "./Components/fleet/counter";
+import Counter from "./Components/Ships/counter";
 
 function App() {
   return <RouterProvider router={router} />;
