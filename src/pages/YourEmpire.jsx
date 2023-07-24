0d99f9f0ec7e57799cef61dace88017eaaf866c4
hel <127905102+candysphynx@users.noreply.github.com>
2023-07-24 09:29:22 +0200

    your empire modifs

--- src/pages/YourEmpire.jsx
+++ src/pages/YourEmpire.jsx
@@ -1,9 +1,22 @@
-
+/**
+    * @description      : 
+    * @author           : 
+    * @group            : 
+    * @created          : 24/07/2023 - 09:08:31
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 24/07/2023
+    * - Author          : 
+    * - Modification    : 
+**/
 import Header from "../models/ModelsHeader";
 import Footer from "../models/ModelsFooter";
 import Menu from "../layout/Menu";
 import Menu from "../Components/Buildings/ViewBuildings";
 import Menu from "../Components/Buildings/CreateBuildings";
+import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
 
 function YourEmpire(){
 
@@ -12,13 +25,13 @@ function YourEmpire(){
         <Header/>
         <Menu/>
         <h1>YOUR EMPIRE</h1>
-            {/* <Ships/> */}
-            {/* <Warehouses/> */}
+            <Ships/>
+            <Warehouses/>
             <Buildings/>  
             <Cbuilding/> 
-            {/* <Battles/> */}      
+            <Battles/>  
         <Footer/>
     </div>
     )
 }
-export default YourEmpire;
+export default YourPage;
