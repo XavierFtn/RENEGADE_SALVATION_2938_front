1e154fb73b8cd4119f7924e85326a8c62586f90b
DiogoCosta82 <132056422+DiogoCosta82@users.noreply.github.com>
2023-07-26 10:26:23 +0200

    Battle en cours

--- src/pages/Battle.jsx
+++ src/pages/Battle.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 
 const Battle = () => {
   const [ships, setShips] = useState({});
+  const [planetarySystems, setPlanetarySystems] = useState([]);
+  const [selectedSystem, setSelectedSystem] = useState(null);
   const token = JSON.parse(sessionStorage.getItem("token")); // Retrieve the token from session storage
 
   // Function to fetch ships data from the backend
@@ -19,28 +21,55 @@ const Battle = () => {
     }
   };
 
-  // Function to handle sending the ships to battle
-  const handleSendShips = async (defenderId) => {
+  // Function to fetch planetary systems data from the backend
+  const fetchPlanetarySystems = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/battle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`, // Include the token in the headers
-        },
-        body: JSON.stringify({ user_id: defenderId }),
-      });
+      const response = await fetch(
+        "http://localhost:8000/api/planetary-systems",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // Include the token in the headers
+          },
+        }
+      );
       const data = await response.json();
-      console.log(data.message); // Battle result message from the backend
-      fetchShips(); // Refresh ships data after the battle
+      setPlanetarySystems(data.planetarySystems);
     } catch (error) {
-      console.error("Erreur lors de l'envoi des vaisseaux:", error);
-      setShips([]); // Set an empty array as a fallback on error
+      console.error(
+        "Erreur lors de la récupération des systèmes planétaires:",
+        error
+      );
     }
   };
 
+  // Function to handle selecting a planetary system as a target
+  const handleSelectSystem = (system) => {
+    setSelectedSystem(system);
+  };
+
+  // // Function to handle sending the ships to battle
+  // const handleSendShips = async (defenderId) => {
+  //   try {
+  //     const response = await fetch("http://127.0.0.1:8000/api/battle", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${token}`, // Include the token in the headers
+  //       },
+  //       body: JSON.stringify({ user_id: defenderId }),
+  //     });
+  //     const data = await response.json();
+  //     console.log(data.message); // Battle result message from the backend
+  //     fetchShips(); // Refresh ships data after the battle
+  //   } catch (error) {
+  //     console.error("Erreur lors de l'envoi des vaisseaux:", error);
+  //     setShips([]); // Set an empty array as a fallback on error
+  //   }
+  // };
+
   useEffect(() => {
     fetchShips();
+    fetchPlanetarySystems();
   }, []);
 
   return (
@@ -49,20 +78,29 @@ const Battle = () => {
       <ul>
         {Object.entries(ships).map(([shipType, quantity]) => (
           <li key={shipType}>
-            Type: {shipType}, Quantity: {quantity}
+            Type: {shipType} - Quantity: {quantity}
           </li>
         ))}
       </ul>
       <h1>Attaque</h1>
-      <button onClick={() => handleSendShips(1)}>
-        Send Ships to Defender 1
-      </button>
-      <button onClick={() => handleSendShips(2)}>
-        Send Ships to Defender 2
-      </button>
-      {/* Add more buttons for other defenders if needed */}
+      <ul>
+        {planetarySystems.map((system) => (
+          <li key={system.id}>
+            {system.name} (X: {system.x_coord}, Y: {system.y_coord})
+            <button onClick={() => handleSelectSystem(system)}>Select</button>
+          </li>
+        ))}
+      </ul>
+      {selectedSystem && (
+        <div>
+          <h2>Selected System</h2>
+          <p>Name: {selectedSystem.name}</p>
+          <p>X Coord: {selectedSystem.x_coord}</p>
+          <p>Y Coord: {selectedSystem.y_coord}</p>
+          {/* Add more information about the selected system */}
+        </div>
+      )}
     </div>
   );
 };
-
 export default Battle;
