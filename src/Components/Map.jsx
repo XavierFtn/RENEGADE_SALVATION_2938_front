b665f0f6ee426fce35b58ec0aebce46d0b2e020f
Montigny Gérald <131920955+Slam85@users.noreply.github.com>
2023-07-26 22:52:58 +0200

    map api?

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,26 +1,46 @@
+import React, { useState, useEffect } from 'react';
 import { Stage, Layer, Circle } from 'react-konva';
-// import radarImage from '../assets/radar.jpg'; 
 
 function Map() {
+  const [user1Coords, setUser1Coords] = useState({ x: 50, y: 60 });
+  const [user2Coords, setUser2Coords] = useState({ x: 150, y: 100 });
+
+  useEffect(() => {
+    // Faites une requête à votre API pour récupérer les coordonnées du premier utilisateur
+    fetch('/api/user1')
+      .then(response => response.json())
+      .then(data => {
+        setUser1Coords({ x: data.x_coord, y: data.y_coord });
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des coordonnées du premier utilisateur:', error);
+      });
+
+    // Faites une requête à votre API pour récupérer les coordonnées du deuxième utilisateur
+    fetch('/api/user2')
+      .then(response => response.json())
+      .then(data => {
+        setUser2Coords({ x: data.x_coord, y: data.y_coord });
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des coordonnées du deuxième utilisateur:', error);
+      });
+  }, []);
+
   return (
     <Stage width={999} height={999}>
       <Layer>
-        <KonvaImage
-          image={radarImage}
-          width={1000}
-          height={1000}
-        />
         <Circle
-          x={532}
-          y={128}
+          x={user1Coords.x}
+          y={user1Coords.y}
           radius={10}
           fill="red"
           stroke="green"
           strokeWidth={1}
         />
         <Circle
-          x={487}
-          y={128}
+          x={user2Coords.x}
+          y={user2Coords.y}
           radius={10}
           fill="blue"
           stroke="black"
