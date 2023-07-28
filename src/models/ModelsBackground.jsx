8d12946a000ac56272678c1c98fb3c0b6e5b9bf3
Jimmy <131156471+Jimbow-cl@users.noreply.github.com>
2023-07-28 13:14:40 +0200

    Spinners

--- src/models/ModelsBackground.jsx
+++ src/models/ModelsBackground.jsx
@@ -3,7 +3,7 @@
 function BackgroundVideo() {
     return (
         <div className="video">
-            <video autoPlay loop muted id="video" controls="false" >
+            <video autoPlay loop muted id="video"  controls={false} >
                 <source src="../src/Components/vid/sfx.mp4" type="video/mp4" />
             </video>
         </div>)
