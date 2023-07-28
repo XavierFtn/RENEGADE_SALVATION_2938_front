bdb0cf8c6c534046ffc46f9607c760fc86326119
Montigny Gérald <131920955+Slam85@users.noreply.github.com>
2023-07-28 14:52:54 +0200

    Battle midle jet

--- src/pages/Battle.jsx
+++ src/pages/Battle.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import Ships from "../Components/Ships/ViewShips";
 const Battle = () => {
     const [ships, setShips] = useState({});
     const [planetarySystems, setPlanetarySystems] = useState([]);
@@ -7,7 +7,7 @@ const Battle = () => {
     const [selectedShips, setSelectedShips] = useState({});
     const [defenderInfo, setDefenderInfo] = useState(null);
     const token = JSON.parse(sessionStorage.getItem("token")); // Retrieve the token from session storage
-
+    const [battleResult, setBattleResult] = useState(null);
     // Function to fetch ships data from the backend
     const fetchShips = async () => {
         try {
@@ -69,7 +69,13 @@ const Battle = () => {
     };
 
     // Function to handle sending the ships to battle
+    // Function to handle sending the ships to attack
     const handleSendShips = async (defenderId) => {
+        if (Object.keys(selectedShips).length === 0) {
+            console.log("Victoire ! Vous avez gagné car vous n'avez pas envoyé de vaisseaux à l'attaque.");
+            return;
+        }
+
         try {
             const response = await fetch("http://127.0.0.1:8000/api/attack", {
                 method: "POST",
@@ -83,13 +89,16 @@ const Battle = () => {
                 }),
             });
             const data = await response.json();
-            console.log(data.message); // Battle result message from the backend
-            fetchShips(); // Refresh ships data after the battle
+            console.log(battleResult, 'result');
+            // Mettre à jour l'état selectedShips avec les résultats de la bataille
+            setBattleResult(data.message);
+
+            // Rafraîchir les données des vaisseaux après la bataille
+            fetchShips();
         } catch (error) {
             console.error("Erreur lors de l'envoi des vaisseaux:", error);
         }
     };
-
     useEffect(() => {
         fetchShips();
         fetchPlanetarySystems();
@@ -97,45 +106,63 @@ const Battle = () => {
 
     return (
         <div>
-            <h1>Flotte</h1>
-            <ul>
-                {ships &&
-                    Object.entries(ships).map(([shipType, quantity]) => (
-                        <li key={shipType}>
-                            Type: {shipType} - Quantity: {quantity}
-                            <input
-                                type="number"
-                                min="0"
-                                max={quantity}
-                                value={selectedShips[shipType] || 0}
-                                onChange={(e) => handleSelectShips(shipType, parseInt(e.target.value))}
-                            />
-                        </li>
-                    ))}
-            </ul>
-            <h1>Attaque</h1>
-            <ul>
-                {planetarySystems &&
-                    planetarySystems.map((system) => (
-                        <li key={system.id}>
-                            {system.name} (X: {system.x_coord}, Y: {system.y_coord})
-                            <button onClick={() => handleSelectSystem(system)}>Select</button>
-                        </li>
-                    ))}
-            </ul>
-            {selectedSystem && (
-                <div>
-                    <h2>Selected System</h2>
-                    <p>Name: {selectedSystem.name}</p>
-                    <p>X Coord: {selectedSystem.x_coord}</p>
-                    <p>Y Coord: {selectedSystem.y_coord}</p>
-                    {defenderInfo && (
-                        <p>Defender: {defenderInfo.name}</p>
-                    )}
-                    {/* Add more information about the selected system */}
-                    <button onClick={() => selectedSystem.user_id && handleSendShips(selectedSystem.user_id)}>Attack</button>
-                </div>
-            )}
+            <div className="py-0 p-1">
+                <h1>Flotte</h1>
+                <ul>
+                    <Ships type={"fighter"} />
+                    <Ships type={"frigate"} />
+                    <Ships type={"cruiser"} />
+                    <Ships type={"destroyer"} />
+                    {ships &&
+                        Object.entries(ships).map(([shipType, quantity]) => (
+                            <li key={shipType}>
+                                Type: {shipType} - Quantity: {quantity}
+                                <input
+                                    type="number"
+                                    min="0"
+                                    max={quantity}
+                                    value={selectedShips[shipType] || 0}
+                                    onChange={(e) => handleSelectShips(shipType, parseInt(e.target.value))}
+                                />
+                            </li>
+                        ))}
+                </ul>
+            </div>
+            <div className="py-0 p-4">
+                <h1>Selected System</h1>
+                <ul>
+                    {planetarySystems &&
+                        planetarySystems.map((system) => (
+                            <li key={system.id}>
+                                {system.name} (X: {system.x_coord}, Y: {system.y_coord})
+                                <button onClick={() => handleSelectSystem(system)}>Select</button>
+                            </li>
+                        ))}
+                </ul>
+            </div>
+            <div className="py-0 p-6">
+                {selectedSystem && (
+                    <div>
+                        <h2>Attaque</h2>
+                        <p>Name: {selectedSystem.name}</p>
+                        <p>X Coord: {selectedSystem.x_coord}</p>
+                        <p>Y Coord: {selectedSystem.y_coord}</p>
+                        {defenderInfo && (
+                            <p>Defender: {defenderInfo.name}</p>
+                        )}
+                        <button onClick={() => selectedSystem.user_id && handleSendShips(selectedSystem.user_id)}>Attack</button>
+                    </div>
+                )}
+            </div>
+            <div className="py-0 p-8">
+                {battleResult && (
+                    <div>
+                        <h2>Résultats de la bataille</h2>
+
+                        <p>{battleResult}</p>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
