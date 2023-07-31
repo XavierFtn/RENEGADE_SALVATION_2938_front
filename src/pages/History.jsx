354a78ebdd50ef97eaf92e20d9e66a25cc4d7ca8
DiogoCosta82 <132056422+DiogoCosta82@users.noreply.github.com>
2023-07-31 09:47:12 +0200

    MJ

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -6,25 +6,25 @@ import { useEffect, useState } from "react";
 import { getRounds } from "../Components/Api/backend_helper";
 
 function History() {
-  const [rounds, setRounds]=useState([]);
+  const [rounds, setRounds] = useState([]);
   useEffect(() => {
     getRounds().then((result) => {
-      console.log('rounds',result.round);
+      console.log("rounds", result.round);
       setRounds(result.round);
     });
-  },[]);
+  }, []);
   return (
     <div className="container-fluid">
       <Header name="Battle History " />
       <div className="col">
-        <div className="row  mb-5">
-            
-        </div>
+        <div className="row  mb-5"></div>
 
         <div className=" col-auto ">
-        <Card><Accordion className="bg-dark"  defaultActiveKey="0">
-           <ViewBattles battle={rounds} />
-          </Accordion></Card>
+          <Card>
+            <Accordion className="bg-dark" defaultActiveKey="0">
+              <ViewBattles battle={rounds} />
+            </Accordion>
+          </Card>
         </div>
       </div>
       <Footer />
