640fb5e61bf1f339fdda51506e474b9a37383875
XavierFtn <xavier.fontaine072@gmail.com>
2023-07-31 13:56:55 +0200

    final commit stripe

--- src/pages/Stripe.jsx
+++ src/pages/Stripe.jsx
@@ -1,18 +1,26 @@
 import { Button, Card } from "react-bootstrap";
 import Footer from "../models/ModelsFooter";
 import Header from "../models/ModelsHeader";
+import { useEffect, useState } from "react";
+import ViewRessources from "../Components/Ressources/ViewRessources";
+import { getRessources } from "../Components/Api/backend_helper";
 
 function Stripe() {
+  const [ressources, setRessources] = useState({});
+
+  useEffect(() => {
+    getRessources().then((result) => setRessources(result));
+  }, []);
   return (
     <div className="container-fluid">
       <Header name="Links Payments " />
-      <div className="col">
+      <div className="col mb-5">
         <div className="row  mb-5"></div>
 
         <div className=" col-auto ">
-          <h1 className="orbitron3">WIP</h1>
           <div className="row">
-            <Card style={{ width: "30rem" }}>
+            <div className=" col-4">
+            <Card className="mb-5" style={{ width: "30rem" }}>
               <Card.Img variant="top" src="src\Components\img\stone.png" />
               <Card.Body>
                 <Card.Title>
@@ -21,15 +29,20 @@ function Stripe() {
                 <Card.Text>
                   <div className="orbitron">1000 Ore</div>
                 </Card.Text>
-                <Button
+                <Button>
                   variant="warning"
                   href="https://buy.stripe.com/test_14kcQ29Tl6kQgtq001"
-                >
+                
+                </Button>
+                <Card.Title className="orbitron2">Buy Ore</Card.Title>
+                <Card.Text className="orbitron5">Price : 1€</Card.Text>
+                <Button className="orbitron5" variant="warning" href="">
                   Payment
                 </Button>
               </Card.Body>
-            </Card>
-            <Card style={{ width: "30rem" }}>
+            </Card></div>
+            <div className=" col-4">
+            <Card className="mb-5" style={{ width: "30rem" }}>
               <Card.Img variant="top" src="src\Components\img\jerrycan.png" />
               <Card.Body>
                 <Card.Title>
@@ -42,10 +55,27 @@ function Stripe() {
                   variant="warning"
                   href="https://buy.stripe.com/test_cN25nAaXp4cI0us4gi"
                 >
+                <Card.Title className="orbitron2">Buy Fuel</Card.Title>
+                <Card.Text className="orbitron5">Price : 1€</Card.Text>
+                <Button className="orbitron5" variant="warning" href="">
                   Payment
                 </Button>
               </Card.Body>
-            </Card>
+            </Card></div>
+            <div className=" col-4">
+            <Card className="mb-5" style={{ width: "30rem" }}>
+              <Card.Img variant="top" src="src\Components\img\energy.jpg" />
+              <Card.Body>
+                <Card.Title className="orbitron2">Buy Energy</Card.Title>
+                <Card.Text className="orbitron5">Price : 1€</Card.Text>
+                <Button className="orbitron5" variant="warning" href="">
+                  Payment
+                </Button>
+              </Card.Body>
+            </Card></div>
+            <div className="row mb-5">
+              <ViewRessources ressources={ressources} />
+            </div>
           </div>
         </div>
       </div>
